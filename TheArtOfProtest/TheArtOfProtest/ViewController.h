55cef512bb377687e3feb7e4983d40ac1072e4c9
HwangKyuman <hwangkyuman@HwangKyumanui-MacBook.local>
2015-07-04 14:53:38 +0900

    Initial Commit

---
+++ TheArtOfProtest/TheArtOfProtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheArtOfProtest
+//
+//  Created by HwangKyuman on 2015. 7. 4..
+//  Copyright (c) 2015년 JinboNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TheArtOfProtest/TheArtOfProtest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TheArtOfProtest
+//
+//  Created by HwangKyuman on 2015. 7. 4..
+//  Copyright (c) 2015년 JinboNet. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
