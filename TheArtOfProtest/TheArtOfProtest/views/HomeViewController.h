4aa0d309d239da5573cc49ef0481b551e250f06a
HwangKyuman <hwangkyuman@HwangKyumanui-MacBook.local>
2015-07-08 00:11:45 +0900

    이전 커밋에서 누락된 파일들 추가

---
+++ TheArtOfProtest/TheArtOfProtest/views/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  TheArtOfProtest
+//
+//  Created by HwangKyuman on 2015. 7. 6..
+//  Copyright (c) 2015년 JinboNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ TheArtOfProtest/TheArtOfProtest/views/MainTabBarController.m
@@ -0,0 +1,37 @@
+//
+//  MainTabBarController.m
+//  TheArtOfProtest
+//
+//  Created by HwangKyuman on 2015. 7. 6..
+//  Copyright (c) 2015년 JinboNet. All rights reserved.
+//
+
+#import "MainTabBarController.h"
+
+@interface MainTabBarController ()
+
+@end
+
+@implementation MainTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
