0599745ee420b51a1618f0cdc2d77d5df72b7313
han0ah <hwangkyuman@HwangKyumanui-MacBook.local>
2015-07-24 01:06:52 +0900

    UITableViewController 기반의 새 매뉴얼 하며 뷰컨트롤러 추가

---
+++ TheArtOfProtest/TheArtOfProtest/views/ManualViewController.h
@@ -0,0 +1,13 @@
+//
+//  ManualViewController.h
+//  TheArtOfProtest
+//
+//  Created by HwangKyuman on 2015. 7. 24..
+//  Copyright (c) 2015년 JinboNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ManualViewController : UITableViewController
+
+@end
