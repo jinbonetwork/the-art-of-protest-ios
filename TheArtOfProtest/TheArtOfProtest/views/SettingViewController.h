500a8afcfb11dbf26bf5b506ff8a97db7a7497eb
han0ah <han0ah92@gmail.com>
2015-08-23 22:12:12 +0900

    임시 정보 화면 추가. 상 하단바 white로 디자인 적용

---
+++ TheArtOfProtest/TheArtOfProtest/views/SettingViewController.h
@@ -0,0 +1,14 @@
+//
+//  SettingViewController.h
+//  TheArtOfProtest
+//
+//  Created by HwangKyuman on 2015. 8. 23..
+//  Copyright (c) 2015년 JinboNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *labelLastModified;
+
+@end
---
+++ TheArtOfProtest/TheArtOfProtest/views/SettingViewController.m
@@ -0,0 +1,41 @@
+//
+//  SettingViewController.m
+//  TheArtOfProtest
+//
+//  Created by HwangKyuman on 2015. 8. 23..
+//  Copyright (c) 2015년 JinboNet. All rights reserved.
+//
+
+#import "SettingViewController.h"
+#import "AOPContentsManager.h"
+
+@interface SettingViewController ()
+
+@end
+
+@implementation SettingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    AOPContentsManager *manager = [AOPContentsManager sharedManager];
+    if (manager.lastModified != nil) {
+        self.labelLastModified.text = manager.lastModified;
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
